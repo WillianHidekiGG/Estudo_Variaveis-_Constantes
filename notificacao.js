f1c8a04cdceb25e35c381344da7f3ad8432b85a7
WillianHideki <willianhidekigg@gmail.com>
2023-10-26 02:10:29 -0300

    Notificações adicionadas

---
+++ notificacao.js
@@ -0,0 +1,5 @@
+//const não pode ser mudado a variavel, que se repete.
+const notificacao = "Olha isso aqui!" 
+
+
+console.log(notificacao + "Você perdeu!")
---
+++ script.js
@@ -0,0 +1,6 @@
+//saida
+console.log("Digite o seu nome")
+//let é cdvariavel que pode mudar
+let nome = "willian"
+//concatenar uma mensagem fixa + uma variavel
+console.log("Bem-vindo " + nome )
